638ed9db1fd5741eb7cb616da04e1c50802dc1b3
Syeda Naher <ssnaher35@gmail.com>
2021-10-30 13:32:21 +0600

    first commit

---
+++ src/Pages/Home/Packages/Packages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Packages.css';
+const Packages = () => {
+    return (
+        <div>
+            <h2>this is our packages</h2>
+        </div>
+    );
+};
+
+export default Packages;
